3584afff42e4f1520aeb653021e01b034c6aa0ed
bankoja <james.bankole99@gmail.com>
2018-08-10 14:32:04 +0000

    Update serial_ communication.py

--- serial_ communication.py
+++ serial_ communication.py
@@ -18,17 +18,16 @@ input=1
 while 1 :
     # get keyboard input
     input = raw_input(">> ")
-        # Python 3 users
-        # input = input(">> ")
+        
+        
     if input == 'exit':
         ser.close()
         exit()
     else:
         # send the character to the device
-        # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
         ser.write(input + '\r\n')
         out = ''
-        # let's wait one second before reading output (let's give device time to answer)
+        # wait one second before reading output 
         time.sleep(1)
         while ser.inWaiting() > 0:
             out += ser.read(1)
